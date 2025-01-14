76290a0b9b1ba2cbfb595286637767489cad00c3
malak238 <notlemon00@gmail.com>
2025-01-14 18:09:28 -0500

    this is my c# code

---
+++ c#_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for their name
+            Console.WriteLine("Please enter your name:");
+            string userName = Console.ReadLine();
+            
+            // Greet the user
+            Console.WriteLine($"Hello, {userName}! Welcome to the world of C#.");
+        }
+    }
+}
